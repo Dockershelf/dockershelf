a984f5da846b10973442d06ccc8c4a46ccbc6978
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2018-06-06 18:29:26 -0400

    [REF] Changing the way that python images are built. [REF] Changing image maintainer email. [REF] Improving test cases.

--- python/README.md
+++ python/README.md
@@ -24,18 +24,22 @@
 
 The Python images are built using a bash script [`python/build-image.sh`](https://github.com/LuisAlejandro/dockershelf/blob/master/python/build-image.sh), you can check it out for details.
 
-Each python release is installed using [Pythonz](http://saghul.github.io/pythonz/).
+Each python release is downloaded from Debian sources. Due to the nature of debian packaging, some releases of Python can only be compiled on specific releases of Debian. Based on that premise, Python will be compiled against the corresponding image and then upgraded to Debian Sid.
 
 We'll explain the overall process here:
 
 1. Built `FROM dockershelf/debian:<release>`.
 2. Labelled according to [label-schema.org](http://label-schema.org).
-3. Install developer tools and build depends to handle the Pythonz install and Python compilation.
-4. Install Pythonz.
-5. Install Python according to version.
-6. Removing unnecessary packages.
-7. Install `pip`.
-8. Shrink image by deleting unnecessary files.
+3. Install developer tools to handle the python source download.
+4. Download Python source with `apt-get source python<release>`.
+5. Parse `Build-Depends` and `Depends` fields.
+6. Install `Build-Depends` and `Depends` packages.
+7. Compile Python using `make -f debian/rules install`.
+8. Uninstall `Build-Depends`, developer tools and orphan packages.
+9. Install Python by copying the compiled files to their corresponding places.
+10. Upgrade image to Debian Sid.
+11. Install `pip`.
+12. Shrink image by deleting unnecessary files.
 
 ## Made with :heart: and :hamburger:
 
--- python/build-image.sh
+++ python/build-image.sh
@@ -33,9 +33,15 @@ SECMIRROR="http://deb.debian.org/debian-security"
 
 # This is the list of python packages from debian that make up a minimal
 # python installation. We will use them later.
-PY_PKGS="${PY_VER_NUM_MINOR_STR} ${PY_VER_NUM_MINOR_STR}-minimal \
-	lib${PY_VER_NUM_MINOR_STR} lib${PY_VER_NUM_MINOR_STR}-stdlib \
-	lib${PY_VER_NUM_MINOR_STR}-minimal"
+if [ "${PY_DEBIAN_SUITE}" == "wheezy-security" ]; then
+    PY_PKGS="${PY_VER_NUM_MINOR_STR}-minimal lib${PY_VER_NUM_MINOR_STR} \
+        ${PY_VER_NUM_MINOR_STR} ${PY_VER_NUM_MINOR_STR}-dev"
+else
+    PY_PKGS="lib${PY_VER_NUM_MINOR_STR}-minimal \
+        ${PY_VER_NUM_MINOR_STR}-minimal lib${PY_VER_NUM_MINOR_STR}-stdlib \
+        lib${PY_VER_NUM_MINOR_STR} ${PY_VER_NUM_MINOR_STR} \
+        lib${PY_VER_NUM_MINOR_STR}-dev ${PY_VER_NUM_MINOR_STR}-dev"
+fi
 
 # These are the folders of a debian python installation that we won't need.
 PY_CLEAN_DIRS="/usr/share/lintian /usr/share/man /usr/share/pixmaps \
@@ -44,116 +50,76 @@ PY_CLEAN_DIRS="/usr/share/lintian /usr/share/man /usr/share/pixmaps \
 # Some tools are needed.
 DPKG_TOOLS_DEPENDS="aptitude deborphan debian-keyring dpkg-dev"
 
-# These options are passed to make because we need to speedup the build.
-DEB_BUILD_OPTIONS="parallel=$( nproc ) nocheck nobench"
-
 # Load helper functions
 source "${BASEDIR}/library.sh"
 
 # Apt: Install tools
 # ------------------------------------------------------------------------------
 # We need to install the packages defined at ${DPKG_TOOLS_DEPENDS} because
-# some commands are needed to download the source code before installing the
-# build dependencies. Also, ${DPKG_BUILD_DEPENDS} must be installed to
-# allow installation of Pythonz and Python.
+# some commands are needed to download and process dependencies.
 
-msginfo "Installing tools, build depends and upgrading image ..."
+msginfo "Installing tools and upgrading image ..."
 cmdretry apt-get update
 cmdretry apt-get -d upgrade
 cmdretry apt-get upgrade
 cmdretry apt-get -d install ${DPKG_TOOLS_DEPENDS}
 cmdretry apt-get install ${DPKG_TOOLS_DEPENDS}
 
-# Python: Download
+# Python: Configure sources
 # ------------------------------------------------------------------------------
-# We will use Debian's internal procedure to download the python source code.
-# Python ${PY_VER_NUM} source code is available on ${PY_DEBIAN_SUITE}, so
-# we will apt-get source it.
-# This will give us all the necessary code to build python. Using this method
-# is recommended as it was coded by a Debian Developer who already knows what
-# he's doing. Not like me.
+# We will use Debian's repository to install the different versions of Python.
 
 msginfo "Configuring /etc/apt/sources.list ..."
 if [ "${PY_DEBIAN_SUITE}" == "wheezy-security" ]; then
     {
-        echo "deb-src ${SECMIRROR} ${PY_DEBIAN_SUITE}/updates main"
-    } > /etc/apt/sources.list.d/python-src.list > /dev/null
-else
+        echo "deb ${MIRROR} wheezy main"
+        echo "deb ${SECMIRROR} wheezy/updates main"
+    } | tee /etc/apt/sources.list.d/python.list > /dev/null
+elif [ "${PY_DEBIAN_SUITE}" != "sid" ]; then
     {
-        echo "deb-src ${MIRROR} ${PY_DEBIAN_SUITE} main"
-    } > /etc/apt/sources.list.d/python-src.list > /dev/null
+        echo "deb ${MIRROR} ${PY_DEBIAN_SUITE} main"
+    } | tee /etc/apt/sources.list.d/python.list > /dev/null
 fi
 
 cmdretry apt-get update
 
-msginfo "Downloading python source ..."
-cd "${PY_SOURCE_TEMPDIR}" && cmdretry apt-get source ${PY_VER_NUM_MINOR_STR}
-
-# This is the only folder that was uncompressed (I hope) by apt-get source.
-# We will use it as our base source directory.
-PY_SOURCE_DIR="$( ls -1d ${PY_SOURCE_TEMPDIR}/*/ | sed 's|/$||' )"
-
-# Apt: Install build and runtime depends
+# Apt: Install runtime dependencies
 # ------------------------------------------------------------------------------
-# Now we use some shell/apt plumbing to get build depends and runtime depends.
+# Now we use some shell/apt plumbing to get runtime dependencies.
 
-msginfo "Installing python build and runtime dependencies ..."
-DPKG_BUILD_DEPENDS="$( apt-get -s build-dep ${PY_VER_NUM_MINOR_STR} | grep "Inst " \
-    | awk '{print $2}' | xargs )"
-DPKG_RUN_DEPENDS="$( aptitude search -F%p $( printf '~RDepends:~n^%s$ ' ${PY_PKGS} ) \
-    | xargs | sed "$( printf 's/\s%s\s/ /g;' ${PY_PKGS} )" )"
-DPKG_DEPENDS="$( printf '%s\n' ${DPKG_BUILD_DEPENDS} ${DPKG_RUN_DEPENDS} \
-    | uniq | xargs )"
+msginfo "Installing python runtime dependencies ..."
+DPKG_RUN_DEPENDS="$( aptitude search -F%p \
+    $( printf '~RDepends:~n^%s$ ' ${PY_PKGS} ) | xargs | \
+    sed "$( printf 's/\s%s\s/ /g;' ${PY_PKGS} )" )"
+DPKG_DEPENDS="$( printf '%s\n' ${DPKG_RUN_DEPENDS} | \
+    uniq | xargs )"
+
 cmdretry apt-get -d install ${DPKG_DEPENDS}
 cmdretry apt-get install ${DPKG_DEPENDS}
 
-# Python: Compilation
+if [ "${PY_DEBIAN_SUITE}" == "jessie" ]; then
+    cmdretry apt-get --allow-remove-essential purge findutils
+    cmdretry apt-get -d -t jessie install findutils
+    cmdretry apt-get -t jessie install findutils
+fi
+
+# Python: Installation
 # ------------------------------------------------------------------------------
-# This is the tricky part: we will use the "clean" and "install" targets of the
-# debian/rules makefile (which are used to build a debian package) to compile
-# our python source code. This will generate a python build tree in the 
-# debian folder which we will later process.
+# We will install the packages listed in ${PY_PKGS}
 
-msginfo "Compiling python ..."
-cd "${PY_SOURCE_DIR}" && \
-    DEB_BUILD_OPTIONS="${DEB_BUILD_OPTIONS}" dpkg-buildpackage --no-sign -b -tc
+msginfo "Installing Python ..."
+cmdretry apt-get -d install ${PY_PKGS}
+cmdretry apt-get install ${PY_PKGS}
 
-ls -lah "${PY_SOURCE_DIR}"
+if [ ! -f "/usr/bin/python" ]; then
+    ln -s /usr/bin/${PY_VER_NUM_MINOR_STR} /usr/bin/python
+fi
 
-exit 1
-
-# Python: Installing
+# Apt: Remove unnecessary packages
 # ------------------------------------------------------------------------------
-# We will use Pythonz to take care of python compilation for us.
-
-# msginfo "Configuring /etc/apt/sources.list ..."
-# # cat > /etc/apt/sources.list.d/libssl.list << EOF
-# # deb http://deb.debian.org/debian jessie-backports main
-# # deb http://deb.debian.org/debian jessie main
-# # EOF
-
-# {
-#     echo "deb [trusted=yes] http://ppa.launchpad.net/deadsnakes/ppa/ubuntu ${UBUNTU_RELEASE} main"
-# } | tee /etc/apt/sources.list.d/python.list > /dev/null
-
-# msginfo "Installing Python ..."
-# cmdretry apt-get update
-# cmdretry apt-get -d install ${PY_VER_NUM_MINOR_STR}
-# cmdretry apt-get install ${PY_VER_NUM_MINOR_STR}
-
-# # rm -rfv /etc/apt/sources.list.d/libssl.list
-# cmdretry apt-get update
-
-# Apt: Remove build depends
-# ------------------------------------------------------------------------------
-# We need to clear the filesystem of unwanted packages before installing python
-# because some files might be confused with already installed python packages.
+# We need to clear the filesystem of unwanted packages to shrink image size.
 
 msginfo "Removing unnecessary packages ..."
-cmdretry apt-get purge $( echo ${DPKG_BUILD_DEPENDS} \
-    | sed "$( printf 's/\s%s\s/ /g;' ${DPKG_RUN_DEPENDS} )" )
-cmdretry apt-get autoremove
-
 # This is clever uh? I figured it out myself, ha!
 cmdretry apt-get purge $( apt-mark showauto $( deborphan -a -n \
                                 --no-show-section --guess-all --libdevel \
@@ -171,14 +137,22 @@ cmdretry apt-get autoremove
 # ------------------------------------------------------------------------------
 # Let's bring in the old reliable pip guy.
 
-msginfo "Installing pip ..."
-if [ "${PY_VER_NUM}" == "3.2" ]; then
-    curl -fsSL "https://bootstrap.pypa.io/3.2/get-pip.py" | ${PY_VER_NUM_MINOR_STR} - 'setuptools<30'
-else
-    curl -fsSL "https://bootstrap.pypa.io/get-pip.py" | ${PY_VER_NUM_MINOR_STR}
-fi
+# msginfo "Installing pip ..."
+# if [ "${PY_VER_NUM}" == "3.2" ]; then
+#     ${PY_VER_NUM_MINOR_STR} -m easy_install setuptools==29.0.1
+#     curl -fsSL "https://bootstrap.pypa.io/3.2/get-pip.py" | \
+#         ${PY_VER_NUM_MINOR_STR} - 'setuptools==29.0.1'
+# elif [ "${PY_VER_NUM}" == "2.6" ]; then
+#     ${PY_VER_NUM_MINOR_STR} -m easy_install setuptools==29.0.1
+#     curl -fsSL "https://bootstrap.pypa.io/2.6/get-pip.py" | \
+#         ${PY_VER_NUM_MINOR_STR} - 'setuptools==29.0.1'
+# else
+#     ${PY_VER_NUM_MINOR_STR} -m easy_install setuptools
+#     curl -fsSL "https://bootstrap.pypa.io/get-pip.py" | \
+#         ${PY_VER_NUM_MINOR_STR}
+# fi
 
-# Changing bash prompt
+# Bash: Changing prompt
 # ------------------------------------------------------------------------------
 # To distinguish images.
 
