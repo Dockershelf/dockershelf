b64866257c4cd9c78c08fcf6aa59568ceb385448
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2019-06-24 15:17:45 -0400

    [FIX] Fixing build.

--- build-image.sh
+++ build-image.sh
@@ -91,9 +91,15 @@ if [ "${DOCKER_IMAGE_TYPE}" == "mongo" ]; then
     cp "${DOCKER_IMAGE_TYPE_DIR}/articles.js"  "${DOCKER_IMAGE_DIR}"
 fi
 
-# Copy entrypoint if we are building Postgres or PHP
-if [ "${DOCKER_IMAGE_TYPE}" == "postgres" ] || [ "${DOCKER_IMAGE_TYPE}" == "php" ]; then
+# Copy entrypoint if we are building Postgres
+if [ "${DOCKER_IMAGE_TYPE}" == "postgres" ]; then
     cp "${DOCKER_IMAGE_TYPE_DIR}/docker-entrypoint.sh"  "${DOCKER_IMAGE_DIR}"
+fi
+
+# Copy entrypoint if we are building PHP
+if [ "${DOCKER_IMAGE_TYPE}" == "php" ]; then
+    cp "${DOCKER_IMAGE_TYPE_DIR}/docker-entrypoint.sh"  "${DOCKER_IMAGE_DIR}"
+    cp "${DOCKER_IMAGE_TYPE_DIR}/apache2-foreground"  "${DOCKER_IMAGE_DIR}"
 fi
 
 # Copy latex sample if we are building Latex
